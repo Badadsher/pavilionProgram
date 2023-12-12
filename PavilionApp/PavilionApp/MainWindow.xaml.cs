89a346dfe2cbbf7b769c4de59db856515ebb6d4f
Joe Biden <dominserv123@mail.ru>
2023-12-12 18:10:39 +0300

    signPage

--- PavilionApp/PavilionApp/MainWindow.xaml.cs
+++ PavilionApp/PavilionApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PavilionApp.View;
 
 namespace PavilionApp
 {
@@ -23,6 +24,8 @@ namespace PavilionApp
         public MainWindow()
         {
             InitializeComponent();
-        }
+            MainFrame.Navigate(new SignIn());        }
+
+     
     }
 }
